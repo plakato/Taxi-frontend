14f278d6e057a0e8ac088fa20afc192e327c8db2
Patul <plakato01@gmail.com>
2017-11-22 19:33:03 +0100

    updated sign in site looks, added custom theme...commiting to upgrade CLI

--- web-dispatching-drivers/src/app/user/user.module.ts
+++ web-dispatching-drivers/src/app/user/user.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-import {MatButtonModule} from '@angular/material';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatGridListModule } from '@angular/material';
 
 
 @NgModule({
   imports: [
     CommonModule,
-     MatButtonModule
+     MatButtonModule,
+     MatFormFieldModule,
+     MatInputModule,
+     MatGridListModule
   ],
   declarations: [LoginComponent]
 })
